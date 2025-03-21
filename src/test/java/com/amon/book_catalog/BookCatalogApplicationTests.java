231cf6cc9273f9f4ae1ed760cb1c535266e4d745
KibetAmon <kibetngetich805@gmail.com>
2025-03-21 10:00:45 +0300

    project setup

---
+++ src/test/java/com/amon/book_catalog/BookCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amon.book_catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
